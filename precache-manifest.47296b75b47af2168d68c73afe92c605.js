1e89f2c34648f9895b596247b1b8d94c412476de
majd eddine ben tahar <majdeddinebentahar@gmail.com>
2020-08-08 11:13:56 +0100

    Updates

---
+++ precache-manifest.47296b75b47af2168d68c73afe92c605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7667d94a2d73e360926bf3aa9f6805",
+    "url": "/react-currency-converter/index.html"
+  },
+  {
+    "revision": "232ffc4d6c2826a79397",
+    "url": "/react-currency-converter/static/css/main.4db11384.chunk.css"
+  },
+  {
+    "revision": "27df85b9c171e0b9ce00",
+    "url": "/react-currency-converter/static/js/2.9c1be809.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-currency-converter/static/js/2.9c1be809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "232ffc4d6c2826a79397",
+    "url": "/react-currency-converter/static/js/main.dc2ba3da.chunk.js"
+  },
+  {
+    "revision": "2357b9f5b8177ff3e719",
+    "url": "/react-currency-converter/static/js/runtime-main.5b3e118e.js"
+  }
+]);
